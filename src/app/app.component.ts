348af428a9bad42ae25c98394e53e0cd9afeefee
codemattcode <codemattcode@gmail.com>
2017-05-13 22:52:27 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  rForm: FormGroup;
+  post:any; // All fields form submitted form
+  message:string='';
+  messageAlert:string = 'Min 10 / Max 1400 characters'; 
+  name:string = '';
+  nameAlert:string = 'Min 3 character';
+  email:string = '';
+  emailAlert:string = 'Min 5 character including @';
+  
+  constructor(private fb: FormBuilder){
+
+    this.rForm = fb.group({
+      'name': [null, Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(100)])],
+      'email': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(100)])],
+      'message': [null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(1400)])]
+    });
+  }
+
+  addPost(post){
+    this.message = post.message;
+    this.name = post.name;
+    this.email = post.email;
+  }
 }
